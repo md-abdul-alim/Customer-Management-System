3dfe5560aee6092b9ad9fbd783e74dc662f0fb20
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-09-30 17:49:20 +0600

    All table data updated

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from .models import *
 # Create your views here.
 
 
-def home(request):
+def index(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
     total_customers = customers.count()
@@ -29,5 +29,13 @@ def products(request):
     return render(request, 'accounts/products.html', context)
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request,pk):
+    customer=Customer.objects.get(id=pk)
+    orders= customer.order_set.all()
+    total_orders = orders.count()
+    context= {
+        'customer':customer,
+        'orders':orders,
+        'total_orders':total_orders
+    }
+    return render(request, 'accounts/customer.html',context)
