097b3bba7034a35a9183f6888462254c0e1098a8
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-10-01 21:36:31 +0600

    User Registration and Login Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,59 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory  # multiple order
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 # Create your views here.
 
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+    
+    if request.method == 'POST':
+        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+            # if user.email == "abdul_alim@zohomail.com":
+            #     #print(user.username)
+            #     login(request,user)
+            #     return redirect('index')
+            # else:
+            #     login(request,user)
+            #     return redirect('register')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return render(request, 'accounts/login.html')
+    
+    return render(request, 'accounts/login.html')
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
 
 def index(request):
     orders = Order.objects.all()
@@ -38,7 +87,7 @@ def customer(request, pk):
     total_orders = orders.count()
 
     myFilter = OrderFilter(request.GET, queryset=orders)
-    orders= myFilter.qs
+    orders = myFilter.qs
 
     context = {
         'customer': customer,
