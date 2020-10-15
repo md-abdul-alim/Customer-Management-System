3c430e13197e4bdc21e5d1d0a1ba653170108b83
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-10-15 10:23:18 +0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -32,12 +32,3 @@ urlpatterns = [
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
          name='password_reset_complete'),
 ]
-
-# template_name='accounts/password_reset.html'
-# template_name='accounts/password_reset_done.html'
-
-# Password reset process
-# 1 - Submit email form                            //PasswordResetView.as_view()
-# 2 - Email sent success message                   //PasswordResetDoneView.as_view()
-# 3 - Link to password Rest form in email          //PasswordResetConfirmView.as_view()
-# 4 - Password successfully changed message        //PasswordResetCompleteView.as_view()
